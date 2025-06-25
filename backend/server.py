217a7b22416aaedddcab1d753096e176e702637f
ali-safa-mohammed <st.alisafaa22@ced.nahrainuniv.edu.iq>
2025-06-25 14:43:54 +0300

    Update server.py
    allow origins changed

--- backend/server.py
+++ backend/server.py
@@ -183,7 +183,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=["https://ali-safa-mohammed.github.io"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
